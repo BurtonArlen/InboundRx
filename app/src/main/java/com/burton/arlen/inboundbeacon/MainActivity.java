ee76fdccc1451c40f827e89b36d6a60594cc5629
arlen <burton.arlen.1990@gmail.com>
2017-07-07 13:49:55 -0700

    roadblock: estimote sdk depriciated

--- app/src/main/java/com/burton/arlen/inboundbeacon/MainActivity.java
+++ app/src/main/java/com/burton/arlen/inboundbeacon/MainActivity.java
@@ -2,15 +2,60 @@ package com.burton.arlen.inboundbeacon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.Region;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.mgmtsdk.feature.settings.api.Beacon;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static android.R.id.list;
+
+public class MainActivity extends AppCompatActivity{
+    private static final Map<String, List<String>> PLACES_BY_BEACONS;
+    static {
+        Map<String, List<String>> placesByBeacons = new HashMap<>();
+        placesByBeacons.put("00000:00000", new ArrayList<String>() {{
+            add("Thing 1");
+            add("Object 2");
+            add("Stuff 3");
+        }});
+        placesByBeacons.put("000:00", new ArrayList<String>() {{
+            add("Thing 1");
+            add("Object 2");
+            add("Stuff 3");
+        }});
+        PLACES_BY_BEACONS = Collections.unmodifiableMap(placesByBeacons);
+    }
+
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    Beacon nearestBeacon = list.get(0);
+                    List<String> places = placesNearBeacon(nearestBeacon);
+                    Log.d("Airport", "Nearest places: " + places);
+                }
+            }
+        });
+        region = new Region("ranged region", UUID.fromString("replace with UUID"), null, null);
     }
 
     @Override
@@ -18,5 +63,25 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback(){
+            @Override
+            public void onServiceReady(){
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause(){
+        beaconManager.stopRanging(region);
+        super.onPause();
+    }
+
+    private List<String> placesNearBeacon(Beacon beacon){
+        String beaconKey = String.format("%d:%d", beacon.getMajor(), beacon.getMinor());
+        if (PLACES_BY_BEACONS.containsKey(beaconKey)){
+            return PLACES_BY_BEACONS.get(beaconKey);
+        }
+        return Collections.emptyList();
     }
 }
