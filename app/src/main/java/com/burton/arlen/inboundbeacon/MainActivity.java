71ef5870ef221342b66e711768134deb01149192
arlen <burton.arlen.1990@gmail.com>
2017-07-07 15:27:28 -0700

    fix imports, resolve roadblock

--- app/src/main/java/com/burton/arlen/inboundbeacon/MainActivity.java
+++ app/src/main/java/com/burton/arlen/inboundbeacon/MainActivity.java
@@ -3,21 +3,15 @@ package com.burton.arlen.inboundbeacon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
-import com.estimote.coresdk.observation.region.Region;
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
 import com.estimote.coresdk.service.BeaconManager;
-import com.estimote.mgmtsdk.feature.settings.api.Beacon;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
-import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity{
     private static final Map<String, List<String>> PLACES_BY_BEACONS;
@@ -47,15 +41,16 @@ public class MainActivity extends AppCompatActivity{
         beaconManager = new BeaconManager(this);
         beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
             @Override
-            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+            public void onBeaconsDiscovered(BeaconRegion region, List<com.estimote.coresdk.recognition.packets.Beacon> list) {
                 if (!list.isEmpty()) {
-                    Beacon nearestBeacon = list.get(0);
+                    com.estimote.coresdk.recognition.packets.Beacon nearestBeacon = list.get(0);
                     List<String> places = placesNearBeacon(nearestBeacon);
+                    // TODO: update the UI here
                     Log.d("Airport", "Nearest places: " + places);
                 }
             }
         });
-        region = new Region("ranged region", UUID.fromString("replace with UUID"), null, null);
+        region = new BeaconRegion("ranged region", UUID.fromString("replace with UUID"), null, null);
     }
 
     @Override
@@ -77,7 +72,7 @@ public class MainActivity extends AppCompatActivity{
         super.onPause();
     }
 
-    private List<String> placesNearBeacon(Beacon beacon){
+    private List<String> placesNearBeacon(com.estimote.coresdk.recognition.packets.Beacon beacon){
         String beaconKey = String.format("%d:%d", beacon.getMajor(), beacon.getMinor());
         if (PLACES_BY_BEACONS.containsKey(beaconKey)){
             return PLACES_BY_BEACONS.get(beaconKey);
